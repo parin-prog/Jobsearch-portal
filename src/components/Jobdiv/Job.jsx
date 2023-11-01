6d1e5ba0f291c5913d58bd7b2ee510e81a2f8d7a
parin-prog <parinvaghani001@gmail.com>
2023-11-01 19:47:40 +0530

    Jobs card

--- src/components/Jobdiv/Job.jsx
+++ src/components/Jobdiv/Job.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { BiTimeFive } from 'react-icons/Bi'
 
 const Job = () => {
   return (
-    <div>Job</div>
+    <div>
+      <div className='jobContainer flex gap-10 justify-center flex-wrap
+        items-center py-10'>
+        <div className='group group/item singleJob w-[250px]
+            p-[20px] bg-white rounded mx-[10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700
+            hover:shadow-lg'>
+          <span className='flex  justify-between items-center gap-4'>
+            <h1 className='text-[16px] font-semibold text-textColor
+                  group-hover:text-white'>Web Developer</h1>
+
+            <span className='flex items-center text-[#ccc] gap-1'>
+              <BiTimeFive />Now
+            </span>
+          </span>
+          <h6 className='text-[#ccc]'>USA</h6>
+
+          <p className='text-[13px] text-[#95959] pt-[20px] border-t-[2px] mt-[20px]
+              group-hover:text-white'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum officiis eum alias, laborum,
+            dolor tenetur quia qui animi.
+          </p>
+
+          <div className='company flex items-center gap-2'>
+            <img src='' alt='Company Logo' className='w-[10%]' />
+            <span className="text-[14px] py-[1rem] block group-hover:text-white">
+              Novac Linus co.</span>
+          </div>
+
+          <button className='border-[2px] rounded-[10px] block p-[10px] w-full text-
+                [14px] font-semibold text-textColor hover:bg-white group-hover/ item:text-textColor 
+                '>
+            Apply Now
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
