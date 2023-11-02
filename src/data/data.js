f58324b6373e7b0e589053e750d7e3ed8edfc4dc
parin-prog <parinvaghani001@gmail.com>
2023-11-02 09:56:45 +0530

    Three Value cards

---
+++ src/data/data.js
@@ -0,0 +1,92 @@
+export const data = [
+	{
+	  "id": 1,
+	  "image": "logo1",
+	  "title": "Web Developer",
+	  "time": "Now",
+	  "location": "Ahmedabad",
+	  "desc": "Lorem vwewvvwe few ewghrg wg",
+	  "company": "Cognizant Linus Co."
+	},
+	{
+	  "id": 2,
+	  "image": "logo2",
+	  "title": "Software Engineer",
+	  "time": "Now",
+	  "location": "Ahmedabad",
+	  "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget eros at nisl aliquet semper. Donec ut ante in quam scelerisque vulputate.",
+	  "company": "TCS Ahmedabad"
+	},
+	{
+	  "id": 3,
+	  "image": "logo3",
+	  "title": "Product Manager",
+	  "time": "Now",
+	  "location": "Ahmedabad",
+	  "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget eros at nisl aliquet semper. Donec ut ante in quam scelerisque vulputate.",
+	  "company": "Infy Ahmedabad"
+	},
+	{
+	  "id": 4,
+	  "image": "logo4",
+	  "title": "Data Scientist",
+	  "time": "Now",
+	  "location": "Ahmedabad",
+	  "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget eros at nisl aliquet semper. Donec ut ante in quam scelerisque vulputate.",
+	  "company": "Accenture Ahmedabad"
+	},
+	{
+	  "id": 5,
+	  "image": "logo5",
+	  "title": "Machine Learning Engineer",
+	  "time": "Now",
+	  "location": "Ahmedabad",
+	  "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget eros at nisl aliquet semper. Donec ut ante in quam scelerisque vulputate.",
+	  "company": "Capgemini Ahmedabad"
+	},
+	{
+		"id": 6,
+		"image": "logo2",
+		"title": "Software Engineer",
+		"time": "Now",
+		"location": "United States",
+		"desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod mauris eget risus feugiat, sit amet cursus tortor, scelerisque.",
+		"company": "Google"
+	      },
+	      {
+		"id": 7,
+		"image": "logo3",
+		"title": "Product Manager",
+		"time": "Next month",
+		"location": "United Kingdom",
+		"desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod mauris eget risus feugiat, sit amet cursus tortor, scelerisque.",
+		"company": "Meta"
+	      },
+	      {
+		"id": 8,
+		"image": "logo4",
+		"title": "Data Scientist",
+		"time": "2 months from now",
+		"location": "Germany",
+		"desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod mauris eget risus feugiat, sit amet cursus tortor, scelerisque.",
+		"company": "Amazon"
+	      },
+	      {
+		"id": 9,
+		"image": "logo5",
+		"title": "UX Designer",
+		"time": "3 months from now",
+		"location": "India",
+		"desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod mauris eget risus feugiat, sit amet cursus tortor, scelerisque.",
+		"company": "Microsoft"
+	      },
+	      {
+		"id": 10,
+		"image": "logo6",
+		"title": "Frontend Engineer",
+		"time": "4 months from now",
+		"location": "France",
+		"desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod mauris eget risus feugiat, sit amet cursus tortor, scelerisque.",
+		"company": "Apple"
+	      }
+      ];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar/Navbar'
 import Search from '../components/Searchdiv/Search'
 import Job from '../components/Jobdiv/Job'
 import Value from '../components/Valuediv/Value'
 import Footer from '../components/Footerdiv/Footer'
+import { data } from '../data/data'
 
 const Home = () => {
+
+  const [job, setJob] = useState(data);
   return (
     <div className='w-[85%] m-auto bg-white'>
       <Navbar />
       <Search />
-      <Job />
+      <div className='jobContainer flex gap-10 flex-wrap items-center py-10'>
+        {job.map((item) => (
+          <Job key={item.id} item={item} />
+        ))}
+      </div>
       <Value />
       <Footer />
     </div>
+
   )
 }
 
